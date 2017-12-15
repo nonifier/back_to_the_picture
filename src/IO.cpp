5bc029786d4bb5cbe43f7b5dab386a27ebbfec83
Olivier Georget <ogeorget@gmail.com>
2017-12-15 21:55:39 +0700

    Fix casting truncation with CV2017

--- src/IO.cpp
+++ src/IO.cpp
@@ -2,10 +2,11 @@
 
 std::string io::to_upper(std::string & s) {
 	std::string upper(s);
-	std::transform(std::cbegin(s), std::cend(s),
+	std::transform(
+		std::cbegin(s), std::cend(s),
 		std::begin(upper), [](int c)
 	{
-		return std::toupper(c);
+		return char(std::toupper(c));
 	});
 	return upper;
 }
