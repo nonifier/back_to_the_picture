bd4ec4b5ea55582ec23cbb5b29d8637720321ade
Olivier Georget <ogeorget@gmail.com>
2017-12-16 19:39:38 +0700

    Fix indentation

--- src/IO.cpp
+++ src/IO.cpp
@@ -5,8 +5,10 @@ std::string io::to_upper(std::string & s)
 	std::string upper(s);
 
 	std::transform(
-		std::cbegin(s), std::cend(s),
-		std::begin(upper), [](int c)
+		std::cbegin(s), 
+		std::cend(s),
+		std::begin(upper), 
+		[](int c)
 	{
 		return char(std::toupper(c));
 	});
